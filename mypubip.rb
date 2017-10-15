3925ec9d4508dcd01c16e11bfb015d09c0fcb1aa
garymalaysia <gtsai70@gmail.com>
2017-10-15 18:15:00 -0400

    added formila

---
+++ mypubip.rb
@@ -0,0 +1,14 @@
+
+
+class Mypubip < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/garymalaysia/myPubip/archive/v1.0.tar.gz"
+  sha256 "67b5f86948b59bdee5c8fd7c1b5dabb1e7c4a051a82121a57c14b650319d7b38"
+
+  def install
+    bin.install "myPubip"    
+    
+  end
+
+end
